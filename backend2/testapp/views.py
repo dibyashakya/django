0647edfb5ed32a00617fb71e95af5c0f49f5479c
dibyashakya <dibyashakya12@gmail.com>
2026-02-03 19:17:05 +0545

    Initial Commit

--- backend2/testapp/views.py
+++ backend2/testapp/views.py
@@ -3,66 +3,6 @@ from django.http import Http404,HttpResponse, HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-# # Create your views here.
-# def index(request):
-#     months = list(monthly_challenges.keys())
-#     return render(request,'testapp/index.html',{
-#         'months':months
-#     })
-
-# def monthly_challenge(request, month):
-#     try:
-#         challenge_text = monthly_challenges[month.lower()]
-#         return HttpResponse(f"<h1>{month} Challenge:{Challenge_text}</h1>")
-#         # return render(request,'testapp/challenge.html',{
-#         #     'text': challenge_text,
-#         #     'month_name': month.capitalize()
-#         # })
-#     except:
-#         return HttpResponseNotFound('<h1>This is not available</h1>')
-#         # raise Http404()
-
-# def monthly_challenge_by_num(request, month):
-#     months = list(monthly_challenges.keys())
-#     redirect_month = months[month - 1]
-#     redirect_url=reverse('monthly_challenge',args=[redirect_month])
-#     return redirect(redirect_url)
-
-# def index(request):
-#     list_items = ""
-#     months = list(monthly_challenges.keys())
-
-#     for month in months:
-#         capitalized_month = month.capitalize()
-#         month_path = reverse("month-challenge", args=[month])
-#         list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-#     # "<li><a href="...">January</a></li><li><a href="...">February</a></li>..."
-
-#     response_data = f"<ul>{list_items}</ul>"
-#     return HttpResponse(response_data)
-#     # return HttpResponse("Welcome to the Challenges App!")
-
-# # def january(request):
-# #     return HttpResponse("January challenge:exercise daily")
-
-
-# def monthly_challenge(request, month):
-#     try:
-#         challenge_text = monthly_challenges[month.lower()]
-#         response_data = render_to_string("challenges/challenge.html")
-#         return HttpResponse(response_data)
-#         # return HttpResponse(response_data, status=200)  # Setting Status Codes
-#     except:
-#         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-
-# # def monthly_challenge_by_number(request, month):
-# #     months = list(monthly_challenges.keys())
-# #     redirect_month = months[month - 1]
-# #     # Using reverse with named URL
-# #     redirect_url = reverse('monthly-challenge', args=[redirect_month])
-# #     return redirect(redirect_url)
-
 
 monthly_challenges = {
     'january': 'Exercise daily for 30 minutes',
@@ -127,10 +67,11 @@ def monthly_challenge(request, month):
         # return HttpResponse(response_data)
         return render(request, "testapp/challenge.html",{
             "text" : challenge_text,
-            " month-name":month.capitalize()
+            " month-name":month
         })
     except KeyError:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
+        # return HttpResponseNotFound("<h1>This month is not supported!</h1>")
     # return HttpResponse(monthly_challenges.get(month, "Invalid month"))
 
 def old_url(request):
